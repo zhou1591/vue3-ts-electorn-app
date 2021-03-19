3ec80ed1a75abf44166b237b83751eda4cb7c8a4
zhoujingsong <zhoujingsong@weimiao.cn>
2021-03-19 19:21:09 +0800

    feat: 踩坑加更新打包配置

--- README.md
+++ README.md
@@ -9,3 +9,15 @@
 作者二维码
 
 <img src='./qr.jpg' />
+
+## 踩坑记录
+
+- package.json 里边不能写build 配置  百度搜索的都是老掉牙的东西
+
+- logo 图标最小 256*256  不明白一共那么一小块地方用那么大干啥
+
+- 配置应该在vue.config.js
+
+- extraResources 这个属性可以把第三方的应用打进去，但是切记不要打包当前目录，他能打一年，然后你会发现硬盘满了
+
+- https://www.easyicon.net/covert/ 转换图标的网站
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,48 @@
+const path = require('path')
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
       // 打包参数配置
       builderOptions: {
-        nsis: {},
-        asar: false,
+        // "extraResources": [],//其他额外的文件
+        "appId": "zjs.code.home",
+        "productName": "周靖松的代码仓库", //  项目名，也是生成的安装文件名，即aDemo.exe
+        "copyright": "zjs-Copyright © 2021", //   版权信息
+        "directories": {
+          "output": "./dist" //  输出文件路径
+        },
+        "electronVersion": "9.3.3", //这个版本牛逼
+        "mac": {
+          "icon": "./logo.icns",
+          "artifactName": "zjs-code-home.${ext}"
+        },
+        "win": {
+          "icon": "./logo.ico",
+          "artifactName": "zjs-code-home.${ext}",
+          "target": [{
+            "target": "nsis",
+            "arch": [
+              "ia32",
+              "x64"
+            ]
+          }],
+          "extraResources": "./lib/*.dll"
+          // "requestedExecutionLevel":"highestAvailable"
+        },
+        "nsis": {
+          "artifactName": "zjs-code-home.${ext}",
+          "oneClick": false, // 一键安装
+          "allowToChangeInstallationDirectory": true, // 更改安装地址
+          "runAfterFinish": true,
+          "perMachine": false,
+          "installerIcon": "./logo.ico", // 安装图标
+          "uninstallerIcon": "./logo.ico", //卸载图标
+          "installerHeaderIcon": "./logo.ico", // 安装时头部图标
+          "createDesktopShortcut": true, // 创建桌面图标
+          "createStartMenuShortcut": true, // 创建开始菜单图标
+          "shortcutName": "周靖松的代码仓库", // 图标名称
+        },
       }
     }
   }
